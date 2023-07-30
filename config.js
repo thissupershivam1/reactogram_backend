a8a728e7d46fb262f35aa4243b192f5c29bc0b1c
shivam <shivammaur59@gmail.com>
2023-07-30 16:27:55 +0530

    completed

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGOBD_URL: "mongodb://localhost:27017",
+    // MONGOBD_URL: "mongodb://localhost:27017",
+    MONGODB_URL:"mongodb+srv://shivammaur59:vcnlDSSHMnPJ0Nt3@cluster0.svvsol9.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
 }
--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require('express');
-const PORT = 4000;
+
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { MONGOBD_URL } = require('./config')
+require("dotenv").config();
+const { MONGODB_URL } = require('./config')
 
 global.__basedir = __dirname;
-mongoose.connect(MONGOBD_URL);
+// mongoose.connect(MONGOBD_URL);
+// mongoose.set('strictQuery', true);
+// mongoose.connection.on('connected', () => {
+//     console.log("DB connected");
+// })
+// mongoose.connection.on('error', (error) => {
+//     console.log("Some error while connecting to DB");
+// })
 
-mongoose.connection.on('connected', () => {
-    console.log("DB connected");
-})
-mongoose.connection.on('error', (error) => {
-    console.log("Some error while connecting to DB");
-})
+mongoose.set('strictQuery', false);
+async function connectToDatabase() {
+    // const MONGODB_URL = 'mongodb://localhost:27017/your_database_name'; // Replace with your MongoDB connection string
+  
+    try {
+      await mongoose.connect(MONGODB_URL, {
+      });
+  
+      console.log('DB connected');
+    } catch (error) {
+      console.error('Error connecting to DB:', error.message);
+    }
+  }
+  
+  mongoose.connection.on('error', (error) => {
+    console.error('MongoDB connection error:', error.message);
+  });
+  
+  // Call the connectToDatabase function to establish the connection
+  connectToDatabase();
 
 require('./models/user_model');
 require('./models/post_model');
@@ -25,6 +47,6 @@ app.use(require('./routes/user_route'));
 app.use(require('./routes/post_route'));
 app.use(require('./routes/file_route'));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT||4000, () => {
     console.log("Server started");
 });
